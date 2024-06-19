fb56ed64a9d440d63c3f187fb3f37193895ffb23
himtechlabs <himtechlabs@gmail.com>
2024-06-19 18:14:00 +0530

    youtube video embed

--- components/About.js
+++ components/About.js
@@ -4,11 +4,13 @@ import React from 'react'
 const About = () => {
   return (
     <section className="d-flex flex-justify-center flex-items-center p-80 flex-col">
-            <span className="icon-wrapper bg-accent text-white rounded"><Image src="/assets/icons/diamond.png" width="26" height="20" alt="" /></span>
-            <h2 className="text-center">Aenean vulputate quis est et pulvinar.</h2>
-            <p className="text-center">Maecenas dapibus turpis id purus mollis aliquam. Sed facilisis nec ipsum nec <br />rutrum.Maecenas dapibus turpis id purus mollis aliquam. Sed facilisis nec <br /> ipsum nec rutrum.Maecenas dapibus turpis id purus mollis aliquam. Sed facilisis nec ipsum nec </p>
-            <button className="btn-primary mt-50">About Us</button>
-           
+        <span className="icon-wrapper bg-accent text-white rounded"><Image src="/assets/icons/diamond.png" width="26" height="20" alt="" /></span>
+        <h2 className="text-center">Aenean vulputate quis est et pulvinar.</h2>
+        <p className="text-center">Maecenas dapibus turpis id purus mollis aliquam. Sed facilisis nec ipsum nec <br />rutrum.Maecenas dapibus turpis id purus mollis aliquam. Sed facilisis nec <br /> ipsum nec rutrum.Maecenas dapibus turpis id purus mollis aliquam. Sed facilisis nec ipsum nec </p>
+        <button className="btn-primary mt-50">About Us</button>
+        <div className="video-container">
+          <iframe src="https://www.youtube.com/embed/LXb3EKWsInQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
     </section>
   )
 }
